8bafde4a6cb08f674c5745f63dc2d52259dd3dc8
Stéphane Lopes <stephane.lopes@uvsq.fr>
2020-03-18 15:47:06 +0100

    Extrait la génération de rapport

---
+++ src/main/kotlin/fr/uvsq/folex/MarkdownReportGenerator.kt
@@ -0,0 +1,92 @@
+package fr.uvsq.folex
+
+import fr.uvsq.folex.Cfg.githubApiUrl
+import fr.uvsq.folex.Cfg.githubToken
+import fr.uvsq.folex.Cfg.repositoryNames
+import java.io.Closeable
+import java.nio.file.Files
+import java.nio.file.Paths
+
+const val MINIMUM_NUMBER_OF_COMMITS = 4
+
+const val MD_OK = ":heavy_check_mark:"
+const val MD_KO = ":x:"
+const val MD_WARNING = ":warning:"
+
+/**
+ * La classe <code>MarkdownReportGenerator</code> permet de générer un rapport au format Markdown.
+ *
+ * @author hal
+ * @version 2020
+ */
+class MarkdownReportGenerator(val reportFilename : String, val students : List<Student>) : Closeable {
+    /**
+     * Fichier de sortie.
+     */
+    private val outputFile = Files.newBufferedWriter(Paths.get(reportFilename))
+
+    /**
+     * Écrit l'entête du tableau.
+     */
+    private fun writeHeader() {
+        var tableHeader = "No. d'étudiant | Nom | Prénom | Github |"
+        var tableSeparator = "-------------- | ----|--------|--------|"
+        for (repository in repositoryNames) {
+            tableHeader += " $repository |"
+            tableSeparator += "----------|"
+        }
+        outputFile.appendln(tableHeader)
+        outputFile.appendln(tableSeparator)
+    }
+
+    /**
+     * Écrit le corps du tableau.
+     */
+    private fun writeBody() {
+        for (student in students) {
+            var mdLine = "${student.studentNo} | ${student.lastname} | ${student.firstname} | "
+
+            if (student.githubLogin.isEmpty()) {
+                mdLine += "$MD_KO |"
+                outputFile.appendln(mdLine)
+                continue
+            }
+
+            val githubQuery = GithubQuery(student.githubLogin, repositoryNames)
+
+            val githubGraphqlRequest = GithubGraphqlRequest(githubApiUrl, githubToken, githubQuery)
+
+            if (githubGraphqlRequest.response.statusCode() == 200) {
+                val repositories = githubGraphqlRequest.parseResponse(repositoryNames)
+
+                if (repositories != null) {
+                    mdLine += "$MD_OK | "
+                    for (repositoryName in repositoryNames) {
+                        mdLine += when (val nbCommits = repositories[repositoryName]) {
+                            0 -> "$MD_KO |"
+                            in 1..MINIMUM_NUMBER_OF_COMMITS -> "$MD_WARNING ($nbCommits)|"
+                            is Int -> "$MD_OK ($nbCommits)|"
+                            else -> "$MD_KO |"
+                        }
+                    }
+                } else { // le compte n'existe pas ou le code de réponse est incorrect
+                    mdLine += "$MD_KO |"
+                }
+            }
+            outputFile.appendln(mdLine)
+        }
+    }
+
+    /**
+     * Génère le rapport.
+     */
+    fun generate() : Unit {
+        writeHeader()
+        writeBody()
+
+    }
+
+    override fun close() {
+        outputFile.close()
+    }
+}
--- src/main/kotlin/fr/uvsq/folex/folex.kt
+++ src/main/kotlin/fr/uvsq/folex/folex.kt
@@ -1,13 +1,6 @@
 package fr.uvsq.folex
 
-import fr.uvsq.folex.Cfg.githubApiUrl
-import fr.uvsq.folex.Cfg.githubToken
-import fr.uvsq.folex.Cfg.repositoryNames
 import fr.uvsq.folex.Cfg.studentFilename
-import java.nio.file.Files
-import java.nio.file.Paths
-
-const val MINIMUM_NUMBER_OF_COMMITS : Int = 4
 
 /**
  * La classe <code>FolexKt</code> est la classe principale de l'application.
@@ -16,51 +9,7 @@ const val MINIMUM_NUMBER_OF_COMMITS : Int = 4
  * @version 2020
  */
 fun main() {
-    val outputFilename = studentFilename.substring(0, studentFilename.lastIndexOf(".")) + ".md"
-    val outputFile = Files.newBufferedWriter(Paths.get(outputFilename))
-
-    var tableHeader = "No. d'étudiant | Nom | Prénom | Github |"
-    var tableSeparator = "-------------- | ----|--------|--------|"
-    for (repository in repositoryNames) {
-        tableHeader += " $repository |"
-        tableSeparator += "----------|"
-    }
-    outputFile.appendln(tableHeader)
-    outputFile.appendln(tableSeparator)
-
     val studentFileParser = StudentFileParser(studentFilename)
-
-    for (student in studentFileParser.students) {
-        var adocLine = "${student.studentNo} | ${student.lastname} | ${student.firstname} | "
-
-        if (student.githubLogin.isEmpty()) {
-            adocLine += ":x: |"
-            outputFile.appendln(adocLine)
-            continue
-        }
-
-        val githubQuery = GithubQuery(student.githubLogin, repositoryNames)
-
-        val githubGraphqlRequest = GithubGraphqlRequest(githubApiUrl, githubToken, githubQuery)
-
-        if (githubGraphqlRequest.response.statusCode() == 200) {
-            val repositories = githubGraphqlRequest.parseResponse(repositoryNames)
-
-            if (repositories != null) {
-                adocLine += ":heavy_check_mark: | "
-                for (repositoryName in repositoryNames) {
-                    adocLine += when (val nbCommits = repositories[repositoryName]) {
-                        0 -> ":x: |"
-                        in 1..MINIMUM_NUMBER_OF_COMMITS -> ":warning: ($nbCommits)|"
-                        is Int -> ":heavy_check_mark: ($nbCommits)|"
-                        else -> ":x: |"
-                    }
-                }
-            } else { // le compte n'existe pas ou le code de réponse est incorrect
-                adocLine += ":x: |"
-            }
-        }
-        outputFile.appendln(adocLine)
-    }
-    outputFile.close()
+    val outputFilename = studentFilename.substring(0, studentFilename.lastIndexOf(".")) + ".md"
+    MarkdownReportGenerator(outputFilename, studentFileParser.students).use { report -> report.generate() }
 }
