eacf006781c2b1c40cd9e24852904af4332f1cb8
Stéphane Lopes <stephane.lopes@uvsq.fr>
2020-03-27 15:18:33 +0100

    Documente la classe Student

--- src/main/kotlin/fr/uvsq/folex/Student.kt
+++ src/main/kotlin/fr/uvsq/folex/Student.kt
@@ -7,7 +7,15 @@ package fr.uvsq.folex
  * @version 2020
  */
 data class Student(val studentNo : String, val lastname : String, val firstname : String, val githubLogin : String) {
+    /**
+     * Dictionnaire contenant le nombre de commits pour chaque dépôt valide de l'étudiant.
+     * Cette attribut est null si le compte gituhb n'existe pas ou si la requête HTTP a retourné un code différent de 200.
+     * Le dictionnaire est vide si aucun dépôt recherché n'existe sur ce compte.
+     */
     var repositories : Map<String, Int>? = null
 
+    /**
+     * Retourne true si l'étudiant a déclaré un compte github valide.
+     */
     fun hasGithubAccount() = !githubLogin.isEmpty()
 }
